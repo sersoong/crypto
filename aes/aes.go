dd3f9c90de82e05d5b333dc89f68b8b891206590
sersoong <sersoong@sersoong.com>
2023-04-28 15:15:49 +0800

    opt

--- aes/aes.go
+++ aes/aes.go
@@ -5,16 +5,19 @@ import (
 	"github.com/gogf/gf/v2/util/grand"
 )
 
+// GenKey Generate a new aes key.
 func GenKey(lenth int) (key string) {
 	key = grand.S(lenth)
 	return
 }
 
+// EncryptAES Encrypt by aes
 func EncryptAES(plaintext []byte, key []byte, iv []byte) (result []byte, err error) {
 	result, err = gaes.Encrypt(plaintext, key, iv)
 	return
 }
 
+// DecryptAES Decrypt by aes
 func DecryptAES(ciphertext []byte, key []byte, iv []byte) (result []byte, err error) {
 	result, err = gaes.Decrypt(ciphertext, key, iv)
 	return
